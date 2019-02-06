9e1deac2a5485e2de99ce3ba90f922f6f24fc90f
ghr85 <ghrenfrew@gmail.com>
2019-02-06 15:48:46 +0000

    Added new view to student model

--- controller.rb
+++ controller.rb
@@ -13,3 +13,13 @@ get '/all-students' do
   @students = Student.all()
 erb(:index)
 end
+
+get '/new' do
+erb(:new)
+end
+
+post '/' do #Why does redirect to / not work?
+  @student = Student.new(params)
+  @student.save
+redirect to '/all-students'
+end
--- db/hogwarts.sql
+++ db/hogwarts.sql
@@ -1,8 +1,15 @@
+DROP TABLE houses;
 DROP TABLE students;
+
+CREATE TABLE houses(
+  id SERIAL4 PRIMARY KEY,
+  house_name VARCHAR(255)
+);
+
 CREATE TABLE students(
   id SERIAL4 PRIMARY KEY,
   first_name VARCHAR(255),
   second_name VARCHAR(255),
-  house VARCHAR(255),
+  house INT4 REFERENCES houses(id),
   age int4
 );
